7211b923951617861378afc7ed5c3caf0ff3e6d3
ottonielmatheus <ottoniel.matheus@gmail.com>
2020-03-28 20:37:43 +0000

    Updates

---
+++ precache-manifest.f405534352b0dc35c50c424f163ea127.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1e55de42bcd466a85a5f5004865c3f",
+    "url": "/flux-control-front/index.html"
+  },
+  {
+    "revision": "0c582cd5e472108282e7",
+    "url": "/flux-control-front/static/css/2.aa9cacd8.chunk.css"
+  },
+  {
+    "revision": "600197af706d7a65d8c6",
+    "url": "/flux-control-front/static/css/main.0dea9774.chunk.css"
+  },
+  {
+    "revision": "0c582cd5e472108282e7",
+    "url": "/flux-control-front/static/js/2.df600082.chunk.js"
+  },
+  {
+    "revision": "4d4505bfadda4b3a85eb805700a321d5",
+    "url": "/flux-control-front/static/js/2.df600082.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "600197af706d7a65d8c6",
+    "url": "/flux-control-front/static/js/main.9259d6c3.chunk.js"
+  },
+  {
+    "revision": "96228bec9fc0afc87dfe",
+    "url": "/flux-control-front/static/js/runtime-main.c68820db.js"
+  }
+]);
